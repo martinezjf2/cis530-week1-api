c4030bc6bfbe633f181c50c0cb55ce17b58958f0
Jeffrey Martinez <martinezjf2@gmail.com>
2026-09-18 22:51:26 -0400

    Initial Week 1 Spring Boot REST API

---
+++ src/main/java/week1/_api/controller/HelloController.java
@@ -0,0 +1,33 @@
+package week1._api.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "api/hello", produces =MediaType.TEXT_PLAIN_VALUE)
+    public String sayHello() {
+        return "Azizian, Welcome to CIS-530 Course!";
+    }
+
+
+    @GetMapping(path="api/info", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> getCourseInfo() {
+        Map<String, Object> courseInfo = new LinkedHashMap<>();
+
+        courseInfo.put("course", "CIS-530");
+        courseInfo.put("week", 1);
+        courseInfo.put("technology", "Spring Boot 4");
+
+
+        return courseInfo;
+    }
+}
